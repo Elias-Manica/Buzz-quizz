21e39396847fa428b005c3dc52b2496dc9ff5ae4
Elias <eliasmanica@poli.ufrj.br>
2022-06-29 15:58:09 -0300

    mostrando todos repositórios da api

---
+++ scriptScreen1.js
@@ -0,0 +1,42 @@
+catchQuizz();
+
+function catchQuizz() {
+  const promisse = axios.get(
+    "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes"
+  );
+
+  promisse.then(showAllQuizz);
+}
+
+function showAllQuizz(object) {
+  let listObjects = object.data;
+  let divAllQuizz = document.querySelector(".allQuizz");
+  let image;
+  let title;
+  let showList = [];
+  divAllQuizz.innerHTML = "";
+  console.log(listObjects);
+  for (let i = 0; i < listObjects.length; i++) {
+    image = listObjects[i].image;
+    title = listObjects[i].title;
+    if (!showList.includes(image) && !showList.includes(title)) {
+      showList.push(image);
+      showList.push(title);
+      divAllQuizz.innerHTML += `
+      <div
+        class="quizz"
+        style="background-image: linear-gradient(
+          180deg,
+          rgba(255, 255, 255, 0) 0%,
+          rgba(0, 0, 0, 0.5) 64.58%,
+          #000000 100%
+        ), url(${listObjects[i].image})"
+        >
+          <p>
+            ${listObjects[i].title}
+          </p>
+      </div>
+    `;
+    }
+  }
+}
