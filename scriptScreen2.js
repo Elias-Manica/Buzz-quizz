2fc38255f5c59c1ee6eb875803051c1df0b54b96
Elias <eliasmanica@poli.ufrj.br>
2022-06-30 21:01:28 -0300

    tela estática fim e botões fim do quizz

--- scriptScreen2.js
+++ scriptScreen2.js
@@ -49,9 +49,20 @@ function putQuestions(array, number) {
               <div class="boxAnswer">
                   
               </div>
-        </div>
+      </div>
       `;
   }
+  allscreen2.innerHTML += `
+  <div class="result">
+    <div class="titleLevel">88% de acerto: Você é praticamente um aluno de Hogwarts!</div>
+    <div class="textLevel">
+      <img src="./imagens/bbb.PNG" />
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </div>
+  </div>
+  <div class="buttomRestart">Reiniciar Quizz</div>
+  <div class="buttomHome">Voltar pra home</div>
+  `;
   let boxAnswer = document.querySelectorAll(".boxAnswer");
   for (let j = 0; j < number; j++) {
     console.log(answerQuizz[j]);
